01ae75fee9eec5f417ae92167c0904e63418290d
Konrad Aksamit <Konrad.Aksamit@microsoft.wsei.edu.pl>
2022-04-01 19:36:02 +0200

    essa

---
+++ lab2/lab2/Classroom.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Classroom
+    {
+        private string name;
+        private Person[] persons;
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public Classroom(string name, Person[] persons)
+        {
+            this.name = name;
+            this.persons = persons;
+        }
+        override public string ToString()
+        {
+            string result = "Classroom: " + this.name + "\n\n";
+            foreach (var p in persons)
+            {
+                result += p.ToString() + "\n";
+            }
+            return result;
+        }
+    }
+}
---
+++ lab2/lab2/Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Teacher : Person
+    {
+        public Teacher(string name, int age) : base(name, age)
+        {
+
+        }
+        override public string ToString()
+        {
+            return "Teacher: " + base.ToString() + "\n";
+        }
+    }
+}
